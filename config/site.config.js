59810bca9dbfaabd10ad05bc491177a864141c98
ybx77 <109066803+ybx77@users.noreply.github.com>
2024-03-29 19:43:03 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -33,7 +33,7 @@ module.exports = {
   protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'dxngzs@163.com',
+  email: '',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
