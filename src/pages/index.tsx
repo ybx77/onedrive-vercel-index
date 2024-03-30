d6aad202bfea6f35aefb9d069466778a96ec14c8
ybx77 <109066803+ybx77@users.noreply.github.com>
2024-03-30 09:31:41 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,16 +7,7 @@ import FileListing from '../components/FileListing'
 import Footer from '../components/Footer'
 import Breadcrumb from '../components/Breadcrumb'
 import SwitchLayout from '../components/SwitchLayout'
-//ceshi 
-const { createRoot } = ReactDOM;
 
-const {  FloatButton  } = antd;
-const App = () => <FloatButton onClick={() => console.log('onClick')} />;
-const ComponentDemo = App;
-
-
-createRoot(mountNode).render(<ComponentDemo />);
-//ceshi
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-white dark:bg-gray-900">
