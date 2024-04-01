a1d4651ff05d509564e6ae34750c0522c47c6ff1
ybx77 <109066803+ybx77@users.noreply.github.com>
2024-04-01 09:13:13 +0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ const createFooterMarkup = () => {
 const Footer = () => {
   return (
     <div
-      className="w-full border-t border-gray-900/10 p-4 text-center text-xs font-medium text-gray-400 dark:border-gray-500/30"
+      className="w-full border-t border-gray-900/10 p-4 text-center text-xs font-medium text-gray-400 dark:border-gray-900/30"
       dangerouslySetInnerHTML={createFooterMarkup()}
     ></div>
   )
