88be455205438d656e6c7ddc3f120403786a8417
ybx77 <109066803+ybx77@users.noreply.github.com>
2024-04-01 14:01:17 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -26,7 +26,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by duoxiaonian. \n<span id="busuanzi_container_site_pv">本站总访问量<span id="busuanzi_value_site_pv"></span>次</span>',
+    'Powered by duoxiaonian. <span id="busuanzi_container_site_pv">本站总访问量<span id="busuanzi_value_site_pv"></span>次</span>',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
